7480339a3f2e4e6935d7be79e40a101108888874
Chumanitikili <ctikili@gmail.com>
2025-03-03 20:01:43 +0200

    Fixed AttributeError by validating data and converting Date

--- stock_predictor.py
+++ stock_predictor.py
@@ -1,1 +1,13 @@
- 
+import yfinance as yf
+import pandas as pd
+import seaborn as sns
+import streamlit as st
+import numpy as np
+from datetime import datetime, timedelta
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
+import plotly.graph_objects as go
+import plotly.express as px
+
+
